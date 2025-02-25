96978215fd10741f5032376b697a3ab98abbe3fd
usiqwerty <benlogin1@protonmail.com>
2025-02-25 16:46:56 +0500

    tasks 1-5

--- index.js
+++ index.js
@@ -4,14 +4,31 @@ const EMPTY = ' ';
 
 const container = document.getElementById('fieldWrapper');
 
+let field = [];
+let currentPlayer = CROSS;
+let gameOver = false;
+let dimension = 3;
 startGame();
 addResetListener();
 
-function startGame () {
-    renderGrid(3);
+
+function generateField(dimension) {
+    field = [];
+    for (let i = 0; i < dimension; i++) {
+        let row = []
+        for (let j = 0; j < dimension; j++) {
+            row.push(EMPTY);
+        }
+        field.push(row);
+    }
 }
 
-function renderGrid (dimension) {
+function startGame() {
+    generateField(dimension);
+    renderGrid(dimension);
+}
+
+function renderGrid(dimension) {
     container.innerHTML = '';
 
     for (let i = 0; i < dimension; i++) {
@@ -26,41 +43,103 @@ function renderGrid (dimension) {
     }
 }
 
-function cellClickHandler (row, col) {
-    // Пиши код тут
-    console.log(`Clicked on cell: ${row}, ${col}`);
+function checkDiagFor(player, cellSelector) {
+    for (let row = 0; row < dimension; row++) {
+        if (field[row][cellSelector(row)] !== player) {
+            return false;
+        }
+    }
+    return true;
+}
 
+function checkWinFor(player) {
+    // по строкам
+    for (const row of field) {
+        if (row.every(cell => cell === player)) {
+            return true;
+        }
+    }
+    // по столбцам
+    for (let col = 0; col < dimension; col++) {
+        if (field.every(row => row[col] === player)) {
+            return true;
+        }
+    }
+    //по главной диагоналям
+    if (checkDiagFor(player, r => r)) {
+        return true;
+    }
+    //по второй диагонали
+    else if (checkDiagFor(player, r => dimension - 1 - r)) {
+        return true;
+    }
+    return false;
+}
+
+function checkGameEnded() {
+    if (checkWinFor(ZERO)) {
+        alert("Нолики выиграли");
+        return true;
+    } else if (checkWinFor(CROSS)) {
+        alert("Крестики выиграли");
+        return true;
+    }
+    for (const row of field) {
+        for (const cell of row) {
+            if (cell === EMPTY) {
+                return false;
+            }
+        }
+    }
+    alert("Победила дружба!");
+    return true;
+}
+
+function cellClickHandler(row, col) {
+    if (gameOver) {
+        return;
+    }
+    console.log(`Clicked on cell: ${row}, ${col}`);
+    if (field[row][col] !== EMPTY) {
+        return;
+    }
+    field[row][col] = currentPlayer;
+    renderSymbolInCell(currentPlayer, row, col);
+    currentPlayer = currentPlayer === CROSS ? ZERO : CROSS;
 
     /* Пользоваться методом для размещения символа в клетке так:
         renderSymbolInCell(ZERO, row, col);
      */
+    gameOver = checkGameEnded();
+
 }
 
-function renderSymbolInCell (symbol, row, col, color = '#333') {
+function renderSymbolInCell(symbol, row, col, color = '#333') {
     const targetCell = findCell(row, col);
 
     targetCell.textContent = symbol;
     targetCell.style.color = color;
 }
 
-function findCell (row, col) {
+function findCell(row, col) {
     const targetRow = container.querySelectorAll('tr')[row];
     return targetRow.querySelectorAll('td')[col];
 }
 
-function addResetListener () {
+function addResetListener() {
     const resetButton = document.getElementById('reset');
     resetButton.addEventListener('click', resetClickHandler);
 }
 
-function resetClickHandler () {
+function resetClickHandler() {
     console.log('reset!');
 }
 
 
 /* Test Function */
+
 /* Победа первого игрока */
-function testWin () {
+function testWin() {
     clickOnCell(0, 2);
     clickOnCell(0, 0);
     clickOnCell(2, 0);
@@ -71,7 +150,7 @@ function testWin () {
 }
 
 /* Ничья */
-function testDraw () {
+function testDraw() {
     clickOnCell(2, 0);
     clickOnCell(1, 0);
     clickOnCell(1, 1);
@@ -84,6 +163,6 @@ function testDraw () {
     clickOnCell(2, 2);
 }
 
-function clickOnCell (row, col) {
+function clickOnCell(row, col) {
     findCell(row, col).click();
 }
